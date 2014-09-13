343b87c894343dec26b349d25153649182529693
saintcross <miao0803@gmail.com>
2014-09-13 10:28:10 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Jingwei MIAO on 9/13/14.
+//  Copyright (c) 2014 Jingwei MIAO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
